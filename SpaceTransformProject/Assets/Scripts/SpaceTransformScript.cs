9ff030e0dac6dfca095d6049c343b5f5e9c4151f
CCorliss02 <ccorliss3876@students.mc3.edu>
2022-03-03 14:48:13 -0500

    Added Unity project.

---
+++ SpaceTransformProject/Assets/Scripts/SpaceTransformScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceTransformScript : MonoBehaviour
+{
+    public Vector2 worldSpacePoint;
+    public Transform localObjectTransform;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.right);
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position, transform.up);
+
+        Gizmos.color = Color.blue;
+
+        Gizmos.DrawSphere(worldSpacePoint, 0.15f);
+
+        localObjectTransform.localPosition = WorldToLocal(worldSpacePoint);
+    }
+
+//    Vector2 LocalToWorld(Vector2 localPoint)
+//    {
+//        Vector2 worldOffset = (transform.right * localPoint.x) + (transform.up * localPoint.y);
+//        return (Vector2)transform.position + worldOffset;
+//    }
+
+    Vector2 WorldToLocal(Vector2 worldPoint)
+    {
+        Vector2 relativePoint = worldPoint - (Vector2)transform.position;
+        float x = Vector2.Dot(relativePoint, transform.right);
+        float y = Vector2.Dot(relativePoint, transform.up);
+
+        return new Vector2(x, y);
+    }
+}
